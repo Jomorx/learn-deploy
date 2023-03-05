0a075a7ce11625f5ec5d868b6f286e9b87728085
huchanghao <2955568032@qq.com>
2023-03-06 01:12:25 +0800

    test 02

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+            count is: {count} asdasdasd
           </button>
         </p>
         <p>
